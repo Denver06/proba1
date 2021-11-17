f09f3583114f2f3efd1dd59e84dae2ff16fbe61c
Denver06 <79630835+Denver06@users.noreply.github.com>
2021-11-17 10:52:32 +0100

    Add files via upload

---
+++ nevek.py
@@ -0,0 +1,10 @@
+lista=["Gábor","Bea","Anita"]
+név=input(print("Írj ide egy keresztnevet: "))
+for a in lista:
+    if a==név:
+        print("Van ilyen név a listában.")
+import random
+lista1=random.randint(10,30)
+lista2=random.randint(10,30)
+lista3=random.randint(10,30)
+print(lista1)
